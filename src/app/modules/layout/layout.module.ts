3cfabcd72be2512d14532591bfffa017e3e3410a
V. Gorohov <adios_rst@mail.ru>
2021-01-06 20:56:47 +0300

    button styling

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -2,10 +2,15 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { SideLayoutComponent } from './side-layout/side-layout.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [SideLayoutComponent],
-  imports: [CommonModule, MatSidenavModule],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatIconModule
+  ],
   exports: [SideLayoutComponent],
 })
 export class LayoutModule {}
--- src/app/modules/layout/side-layout/side-layout.component.ts
+++ src/app/modules/layout/side-layout/side-layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
   selector: "app-side-layout",
@@ -7,8 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideLayoutComponent implements OnInit {
   showFiller = false;
+  deviceInfo = null;
 
-  constructor() {}
+  constructor(
+    private deviceService: DeviceDetectorService
+  ) {
+    this.epicFunction();
+  }
+
+  epicFunction() {
+    console.log('hello `Home` component');
+    this.deviceInfo = this.deviceService.getDeviceInfo();
+    const isMobile = this.deviceService.isMobile();
+    const isTablet = this.deviceService.isTablet();
+    const isDesktopDevice = this.deviceService.isDesktop();
+    console.log(this.deviceInfo);
+    console.log(isMobile);  // returns if the device is a mobile device (android / iPhone / windows-phone etc)
+    console.log(isTablet);  // returns if the device us a tablet (iPad etc)
+    console.log(isDesktopDevice); // returns if the app is running on a Desktop browser.
+  }
 
   ngOnInit() {}
 }
